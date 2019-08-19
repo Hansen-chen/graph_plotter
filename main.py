a979e4f4fe299c0170e9884b5c49c663610e0fce
hansenchen <hansenchan1995@gmail.com>
2019-08-19 13:50:08 +0800

    backtest markup found, need to find TA library I/O and backtest calculation

--- main.py
+++ main.py
@@ -18,7 +18,29 @@ trace = go.Candlestick(x=df['Date'],
                 close=df['AAPL.Close'])
 data = [trace]
 
+fig=go.Figure(data)
+
+fig.layout.update(
+    title='Backtest Result',
+    yaxis_title='AAPL Stock',
+    shapes = [dict(
+        x0='2016-12-09', x1='2016-12-09', y0=0, y1=1, xref='x', yref='paper',
+        line_width=2),
+        dict(
+        x0='2017-02-02', x1='2017-02-02', y0=0, y1=1, xref='x', yref='paper',
+        line_width=2)
+    ],
+    annotations=[dict(
+        x='2016-12-09', y=0.05, xref='x', yref='paper',
+        showarrow=False, xanchor='left', text='Buy Close Price'),
+        dict(
+        x='2017-02-02', y=0.05, xref='x', yref='paper',
+        showarrow=False, xanchor='left', text='Sell Close Price')
+    ]
+)
+
+
 py.offline.init_notebook_mode(connected=True)
 
 #html file rename needed
-py.offline.plot(data, auto_open=True)
+py.offline.plot(fig, auto_open=True)
